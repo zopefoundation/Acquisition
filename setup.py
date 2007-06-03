5a494467cd294ae1e018412646ae6ac07d50d7e6
Philipp von Weitershausen <philipp@weitershausen.de>
2007-06-03 14:48:24 +0000

    First try at an Acquisition egg

---
+++ setup.py
@@ -0,0 +1,40 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for the Acquisition egg package
+"""
+import os
+from setuptools import setup, find_packages, Extension
+
+setup(name='Acquisition',
+      version = '2.11.0a1',
+      url='http://svn.zope.org/Acquisition',
+      license='ZPL 2.1',
+      description='',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description='The Security framework provides a generic mechanism '
+                       'to implement security policies on Python objects.',
+      
+	  packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+
+      ext_modules=[Extension("Acquistion._Acquisition",
+                             [os.path.join('src', 'Acquisition',
+                                           '_Acquisition.c')
+                              ], include_dirs=['include']),
+                   ],
+      install_requires=['zope.interface'],
+      include_package_data = True,
+      zip_safe = False,
+      )

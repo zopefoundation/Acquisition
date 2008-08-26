f6a0c061ef43b32e8b06ea8f7a876fcc05a9307f
Hanno Schlichting <hanno@hannosch.eu>
2008-08-26 15:52:55 +0000

    We are version 2.12.0dev

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='Acquisition',
-      version = '2.11.0dev',
+      version = '2.12.0dev',
       url='http://cheeseshop.python.org/pypi/Acquisition',
       license='ZPL 2.1',
       description="Acquisition is a mechanism that allows objects to obtain "

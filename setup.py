37298707aa5098d21d59e30783a52466b4e3d27d
Hanno Schlichting <hanno@hannosch.eu>
2009-01-24 23:53:31 +0000

    Back to development

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='Acquisition',
-      version = '2.12.0a1',
+      version = '2.12.0dev',
       url='http://pypi.python.org/pypi/Acquisition',
       license='ZPL 2.1',
       description="Acquisition is a mechanism that allows objects to obtain "

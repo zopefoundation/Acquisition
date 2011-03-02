6a2e176e0a8e7335677e79929a5a18c2c76efc8d
Hanno Schlichting <hanno@hannosch.eu>
2011-03-02 21:22:44 +0000

    Prepare Acquisition 2.13.7.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='Acquisition',
-      version = '2.13.7dev',
+      version = '2.13.7',
       url='http://pypi.python.org/pypi/Acquisition',
       license='ZPL 2.1',
       description="Acquisition is a mechanism that allows objects to obtain "

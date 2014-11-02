bd526118f929082e5ccca099886ed8cacf853ebd
Hanno Schlichting <hanno@hannosch.eu>
2014-11-02 12:34:39 +0100

    vb

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('CHANGES.rst') as f:
 
 setup(
     name='Acquisition',
-    version='4.0.3',
+    version='4.0.4',
     url='http://pypi.python.org/pypi/Acquisition',
     license='ZPL 2.1',
     description="Acquisition is a mechanism that allows objects to obtain "

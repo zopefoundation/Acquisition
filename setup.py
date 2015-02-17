d496ba709a4d7ed774a33cab107887a8cf43187e
Tres Seaver <tseaver@palladion.com>
2015-02-17 12:43:08 -0500

    svb
    [ci skip]

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='Acquisition',
-      version = '2.13.9',
+      version = '2.13.10.dev0',
       url='http://pypi.python.org/pypi/Acquisition',
       license='ZPL 2.1',
       description="Acquisition is a mechanism that allows objects to obtain "

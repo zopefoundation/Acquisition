a27312ab9bb8016954922379c292057fcfc8c25e
Jason Madden <jamadden@gmail.com>
2015-03-28 18:05:46 -0500

    Per @davisagli bump version number in setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ else:
 
 setup(
     name='Acquisition',
-    version='4.1',
+    version='4.2.dev.0',
     url='https://github.com/zopefoundation/Acquisition',
     license='ZPL 2.1',
     description="Acquisition is a mechanism that allows objects to obtain "
